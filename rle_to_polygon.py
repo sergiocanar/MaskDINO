ef04a6b892a530f32aaad15582fbda72fe914e4c
sergiocanar <s.canar@uniandes.edu.co>
2025-09-18 03:15:54 +0000

    add rle2polygon and utils to repo

---
+++ rle_to_polygon.py
@@ -0,0 +1,64 @@
+import os
+import cv2
+import json
+import argparse
+import numpy as np
+from tqdm import tqdm
+from pycocotools import mask
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", type=str, required=True)
+args = parser.parse_args()
+
+def load_json(coco_json_path: str):
+    with open(coco_json_path, 'r') as f:
+        data = json.load(f)
+    
+    return data
+
+def rle_to_polygon(coco_json_path, output_json_path):
+    """
+    Convert RLE annotations to polygon format in a COCO JSON.
+
+    Parameters:
+    - coco_json_path: path to the input COCO JSON with RLE annotations.
+    - output_json_path: path to save the modified COCO JSON with polygon annotations.
+    """
+    # Load the COCO JSON
+    data = load_json(coco_json_path)
+
+    # Iterate through the annotations
+    for ann in tqdm(data['annotations']):
+        if 'segmentation' in ann and type(ann['segmentation']) is not list:
+            
+            # Decode RLE to binary mask
+            binary_mask = mask.decode(ann['segmentation'])
+            
+            # Convert binary mask to contours using OpenCV
+            contours, _ = cv2.findContours(binary_mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            
+            segmentation = []
+            for contour in contours:
+                contour = contour.squeeze().ravel().tolist()
+
+                # Exclude invalid polygons
+                if len(contour)%2 == 0 and len(contour) >= 6:
+                    segmentation.append(contour)
+            
+            # Replace RLE with polygon
+            ann['segmentation'] = segmentation
+
+    # Save the modified COCO JSON
+    with open(output_json_path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == "__main__":
+    # Parse for sages annotation files
+    for split in ['train','val','test']:
+        input_coco_json = os.path.join(args.data_path, f"{split}_annotation_coco.json")
+        output_coco_json = os.path.join(args.data_path, f"{split}_annotation_coco_polygon.json")
+        rle_to_polygon(input_coco_json, output_coco_json)
+    
+    # input_coco_json = os.path.join(args.data_path, f"annotations_C.json")
+    # output_coco_json = os.path.join(args.data_path, f"annotations_C_coco_polygon.json")
+    # rle_to_polygon(input_coco_json, output_coco_json)
