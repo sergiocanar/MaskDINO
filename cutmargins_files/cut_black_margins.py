2fe5891250f6bb16851222e20dc6687572dafc89
sergiocanar <scanar@uniandes.edu.co>
2025-10-15 21:09:11 +0000

    black_margins_algorithm works :)

--- cutmargins_files/cut_black_margins.py
+++ cutmargins_files/cut_black_margins.py
@@ -9,47 +9,53 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import cv2
+import numpy as np
 from tqdm import tqdm
 from os.path import join as path_join
+from skimage.morphology import binary_erosion, disk
 from utils import load_json, create_directory_if_not_exists
 
-def filter_black(image, debug_dir: str = None, debug_counter: int = 0):
-    binary_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, binary_image2 = cv2.threshold(binary_image, 15, 255, cv2.THRESH_BINARY)
-    binary_image2 = cv2.medianBlur(binary_image2, 19)
+def filter_black_endo2023(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    binary_image = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 31, 5)
+    inv_img = cv2.bitwise_not(binary_image)
+    
+    #Mathematical morphology
+    disk_3 = disk(3)
+    binary_image2 = binary_erosion(inv_img, disk_3) 
+    
     x, y = binary_image2.shape[:2]
 
     edges_x, edges_y = [], []
+
     for i in range(x):
-        for j in range(10, y - 10):
+        for j in range(y):
             if binary_image2.item(i, j) != 0:
                 edges_x.append(i)
                 edges_y.append(j)
 
+    crop_coords = None
+    pre1_picture = None
+    
     if not edges_x:
-        print("Empty edge list → skipping crop")
-        final_path = path_join(debug_dir, f'debug_empty_{debug_counter}.png')
-        cv2.imwrite(final_path, image)
-        return image
+        return image, None
+    else:
+        left, right = min(edges_x), max(edges_x)
+        bottom, top = min(edges_y), max(edges_y)
+        crop_coords = (left, right, bottom, top)
+        pre1_picture = image[left:right, bottom:top]
+        
+        edges_x = np.array(edges_x)
+        edges_y = np.array(edges_y)
+                    
+    return pre1_picture, crop_coords
 
-    left, right = min(edges_x), max(edges_x)
-    bottom, top = min(edges_y), max(edges_y)
-
-    print(f"Detected crop → rows: {left}-{right}, cols: {bottom}-{top}, "
-          f"height: {right-left}, width: {top-bottom}")
-
-    pre1_picture = image[left:right, bottom:top]
-    return pre1_picture
-
-
-
-def process_image(frame_src: str, save_path: str, debug_dir: str = None, debug_counter: int = 0):
+def process_image(frame_src: str, save_path: str):
     file_name = os.path.basename(frame_src)
     frame_out_path = os.path.join(save_path, file_name)
     frame = cv2.imread(frame_src)
-    frame = filter_black(frame, debug_dir=debug_dir, debug_counter=debug_counter)
+    frame, _ = filter_black_endo2023(frame)
     cv2.imwrite(frame_out_path, frame)
-
 
 if __name__ == "__main__":
     
@@ -85,8 +91,6 @@ if __name__ == "__main__":
             create_directory_if_not_exists(save_path)
             
             process_image(frame_src=source_path,
-                        save_path=save_path,
-                        debug_dir=debug_dir,
-                        debug_counter=debug_counter)
+                        save_path=save_path)
             debug_counter += 1
             pbar.update(1)
