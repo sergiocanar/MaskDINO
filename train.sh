a83cb33d1101ce319e9ebc70abc2d0bda4651814
sergiocanar <scanar@uniandes.edu.co>
2025-10-24 14:01:49 +0000

    update .sh files

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=0,2,3 python train_net.py --num-gpus 3 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_net.py --num-gpus 4 \
 --config-file configs/endoscapes-cutted/instance-segmentation/swin/maskdino_R50_bs16_50ep_4s_dowsample1_2048.yaml \
 --annots_dir annotations_201 \
-OUTPUT_DIR outputs/Endoscapes2023_new_cutmargins_annotations_201_lr_0_00001_train \
+OUTPUT_DIR outputs/endoscapes2023_cutmargins/annotations_201_new_weights_lr_0_00001_train \
 MODEL.WEIGHTS weights/maskdino_swinl_COCO.pth
